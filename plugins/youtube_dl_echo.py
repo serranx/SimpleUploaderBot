3312bf54294f4666bcf2e72eebd346791f4d5c07
wywxz <53309090+wywxz@users.noreply.github.com>
2022-06-17 21:42:04 -0400

    update youtube_dl_echo.py

--- plugins/youtube_dl_echo.py
+++ plugins/youtube_dl_echo.py
@@ -151,11 +151,11 @@ async def echo(bot, update):
                 if format_string is not None and not "audio only" in format_string:
                     ikeyboard = [
                         InlineKeyboardButton(
-                             "🎥 video " + format_string.split("-")[0] + " " + approx_file_size + " ",
+                             "🎥 video " + format_string.split("-")[0] + " " + approx_file_size,
                             callback_data=(cb_string_video).encode("UTF-8")
                         ),
                         InlineKeyboardButton(
-                            "📄 file " + format_ext + " " + approx_file_size + " ",
+                            "📄 file " + format_ext + " " + approx_file_size,
                             callback_data=(cb_string_file).encode("UTF-8")
                         )
                     ]
