d383c66135c3329d9236b3f0ed3baafbd82ed878
wywxz <53309090+wywxz@users.noreply.github.com>
2022-06-18 17:58:57 -0400

    update mediafire.py

--- plugins/mediafire.py
+++ plugins/mediafire.py
@@ -1,7 +1,6 @@
 import re
 import bs4
 import requests
-import user_agent
 
 def get(url):
     if re.match("download[0-9]*\.mediafire\.com", url.lstrip("https://").lstrip("http://").split("/")[0]):
@@ -20,7 +19,7 @@ def get(url):
         raise Exception("No se encontro ningun link de descarga")
 
     session = requests.Session()
-    session.headers["User-Agent"] = user_agent.generate_user_agent()
+    session.headers["User-Agent"] = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:101.0) Gecko/20100101 Firefox/101.0"
 
     data = session.get(f"https://www.mediafire.com/file/{unique_id}/")
     wrp  = bs4.BeautifulSoup(data.text, "html.parser")
