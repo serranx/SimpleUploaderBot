eb80bcf6b9da03cca5bd1cd76e6b7f0227de09dd
wywxz <53309090+wywxz@users.noreply.github.com>
2022-06-19 11:32:09 -0400

    update help_text.py

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -27,8 +27,10 @@ import user_agent
 @Clinton.on_message(filters.private & filters.command(["mediafire"]))
 async def mediafire_dl(bot, update):
     url = update.text.split()[1]
-    headers = {'User-Agent': str(user_agent.generate_user_agent())}
-    htmlContent = requests.get(url, headers=headers)
+    headers = {"User-Agent": str(user_agent.generate_user_agent())}
+    response = await requests.get(url, headers=headers)
+    print(response.text)
+    """
     await bot.send_message(
         chat_id=update.chat.id,
         text=htmlContent.text,
@@ -36,6 +38,7 @@ async def mediafire_dl(bot, update):
         disable_web_page_preview=True,
         reply_to_message_id=update.message_id
     )
+    """
     
 @Clinton.on_message(filters.reply & filters.text)
 async def edit_caption(bot, update):
