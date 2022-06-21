f599dd2d958201e25da7e6dfb2e4c473be861eba
wywxz <53309090+wywxz@users.noreply.github.com>
2022-06-21 10:13:48 -0400

    update mediafire.py

--- plugins/mediafire.py
+++ plugins/mediafire.py
@@ -204,9 +204,9 @@ async def download_coroutine(bot, session, url, file_name, chat_id, message_id, 
         await bot.edit_message_text(
             chat_id,
             message_id,
-            text="""Initiating Download
+            text="Initiating Download
 URL: {}
-File Size: {}""".format(url, humanbytes(total_length))
+File Size: {}".format(url, humanbytes(total_length))
         )
         with open(file_name, "wb") as f_handle:
             while True:
@@ -225,11 +225,11 @@ File Size: {}""".format(url, humanbytes(total_length))
                         (total_length - downloaded) / speed) * 1000
                     estimated_total_time = elapsed_time + time_to_completion
                     try:
-                        current_message = """**Download Status**
+                        current_message = "**Download Status**
 URL: {}
 File Size: {}
 Downloaded: {}
-ETA: {}""".format(
+ETA: {}.format(
     url,
     humanbytes(total_length),
     humanbytes(downloaded),
