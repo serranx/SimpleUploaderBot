1aa3bbfe9e0554b96b1b6e87b58257b0a9598823
wywxz <53309090+wywxz@users.noreply.github.com>
2022-06-22 10:35:34 -0400

    update youtube_dl_echo.py

--- plugins/youtube_dl_echo.py
+++ plugins/youtube_dl_echo.py
@@ -32,8 +32,8 @@ async def echo(bot, update):
     youtube_dl_password = None
     file_name = None
     url = update.text
-    if "*" in url:
-        url_parts = url.split("*")
+    if " * " in url:
+        url_parts = url.split(" * ")
         if len(url_parts) == 2:
             url = url_parts[0]
             file_name = url_parts[1]
