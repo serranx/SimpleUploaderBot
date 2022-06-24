01e5925aa32836f8f478c71407a2d01ece1dc90c
wywxz <53309090+wywxz@users.noreply.github.com>
2022-06-23 21:48:19 -0400

    update fembed.py

--- plugins/fembed.py
+++ plugins/fembed.py
@@ -81,7 +81,7 @@ async def download(bot, update, formats):
             )
             return False
     if os.path.exists(download_directory):
-        save_ytdl_json_path = Config.DOWNLOAD_LOCATION + "/" + str(update.message.chat.id) + ".json"
+        save_ytdl_json_path = Config.DOWNLOAD_LOCATION + "/" + str(update.chat.id) + ".json"
         if os.path.exists(save_ytdl_json_path):
             os.remove(save_ytdl_json_path)
         end_one = datetime.now()
