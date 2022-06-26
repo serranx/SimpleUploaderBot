26ab4a180f2b6b01062f2281310a040735d67436
wywxz <53309090+wywxz@users.noreply.github.com>
2022-06-25 20:24:07 -0400

    update dl_button.py

--- plugins/dl_button.py
+++ plugins/dl_button.py
@@ -1,8 +1,4 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-# (c) Shrimadhav U K | Modifieded By : @DC4_WARRIOR
 
-# the logging things
 import logging
 logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
