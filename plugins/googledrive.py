e1d3e9076d390b0f25c5beb5f438706a1e3b4b1b
wywxz <53309090+wywxz@users.noreply.github.com>
2022-06-29 13:13:49 -0400

    update googledrive.py

--- plugins/googledrive.py
+++ plugins/googledrive.py
@@ -19,7 +19,7 @@ from hachoir.parser import createParser
 async def get(url):
     req = requests.get(url)
     sp = BeautifulSoup(req.text,"html.parser")
-    file_name = sp.find("meta", {"property": "og:title"}).attrs["content"]
+    file_name = sp.find("a").get_text()
     file_id = url.split("/")[-2]
     file_url = get_direct_url(file_id)
     return {"file_name":file_name, "file_id":file_id, "file_url":file_url}
