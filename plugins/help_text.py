a85c30d257cb25c2aa049cb17bc40d30de8a0934
wywxz <53309090+wywxz@users.noreply.github.com>
2022-07-24 21:44:03 -0400

    update help_text.py

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -59,7 +59,7 @@ async def test(bot, update):
     )
     """
 
-@Clinton.on_message(filters.regex(pattern="fembed\.com|fembed-hd\.com|femax20\.com|vanfem\.com|suzihaza\.com|owodeuwu\.xyz"))
+@Clinton.on_message(filters.regex(pattern="fembed\.com|fembed-hd\.com|femax20\.com|vanfem\.com|suzihaza\.com|owodeuwu\.xyz|plusto\.link"))
 async def dl_fembed(bot, update):
     processing = await update.reply_text("<b>Processing... ⏳</b>", reply_to_message_id=update.message_id)
     
@@ -107,7 +107,7 @@ async def dl_fembed(bot, update):
     
 @Clinton.on_message(filters.regex(pattern="\.mediafire\.com/"))
 async def dl_mediafire(bot, update):
-    video_formats = ["mp4", "mkv", "webm", "avi", "wmv"]
+    video_formats = ["mp4", "mkv", "webm", "avi", "wmv", "mov"]
     audio_formats = ["mp3", "m4a"]
     processing = await update.reply_text("<b>Processing... ⏳</b>", reply_to_message_id=update.message_id)
     if " * " in update.text:
