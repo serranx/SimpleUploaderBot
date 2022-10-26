e5a7677b3fb7deceed9f26cb08a628cfb2c41e1c
wywxz <53309090+wywxz@users.noreply.github.com>
2022-10-26 19:27:45 -0400

    update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-# (c) Shrimadhav U K | Modifieded By : @DC4_WARRIOR
 
 import os
 from config import Config
@@ -11,9 +8,10 @@ if __name__ == "__main__" :
     if not os.path.isdir(Config.DOWNLOAD_LOCATION):
         os.makedirs(Config.DOWNLOAD_LOCATION)
     plugins = dict(root="plugins")
-    Warrior = Clinton("@SimpleUploaderV2Bot",
-    bot_token=Config.BOT_TOKEN,
-    api_id=Config.API_ID,
-    api_hash=Config.API_HASH,
-    plugins=plugins)
+    Warrior = Clinton("@SimpleUploaderBot",
+        bot_token=Config.BOT_TOKEN,
+        api_id=Config.API_ID,
+        api_hash=Config.API_HASH,
+        plugins=plugins
+    )
     Warrior.run()
